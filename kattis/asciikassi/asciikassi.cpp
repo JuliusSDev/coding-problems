8184fa5b757d04083c54c59ce36121250777a077
JuliusSDev <julius.spiegelhalter@googlemail.com>
2024-07-21 23:34:01 +0200

    make format

--- kattis/asciikassi/asciikassi.cpp
+++ kattis/asciikassi/asciikassi.cpp
@@ -1,27 +1,28 @@
-#include <iostream>
 #include <string.h>
+
+#include <iostream>
 using namespace std;
 
-void printFirstLast(uint32_t n){
+void printFirstLast(uint32_t n) {
     cout << "+";
-    for(int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++) {
         cout << "-";
     }
     cout << "+" << endl;
 }
 
-void printMiddleLine(uint32_t n){
+void printMiddleLine(uint32_t n) {
     cout << "|";
-    for(int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++) {
         cout << " ";
     }
     cout << "|" << endl;
 }
-int main(void){
+int main(void) {
     uint32_t n = 0;
     cin >> n;
     printFirstLast(n);
-    for(int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++) {
         printMiddleLine(n);
     }
     printFirstLast(n);
--- kattis/ekkidaudi/ekkidaudi.cpp
+++ kattis/ekkidaudi/ekkidaudi.cpp
@@ -1,29 +1,31 @@
-#include <iostream>
 #include <string.h>
+
+#include <iostream>
 using namespace std;
 
-int main(){
+int main() {
     string inputLine1 = "";
     string inputLine2 = "";
     getline(cin, inputLine1);
     getline(cin, inputLine2);
     string subresult[4];
-    
+
     int whichLine = 0;
-    for(char c : inputLine1){
-        if(c == '|'){
+    for (char c : inputLine1) {
+        if (c == '|') {
             whichLine++;
         } else {
             subresult[whichLine] += c;
         }
     }
     whichLine++;
-    for(char c : inputLine2){
-        if(c == '|'){
+    for (char c : inputLine2) {
+        if (c == '|') {
             whichLine++;
         } else {
             subresult[whichLine] += c;
         }
     }
-    cout << subresult[0] << subresult[2] << " " <<subresult[1] << subresult[3] << endl;
+    cout << subresult[0] << subresult[2] << " " << subresult[1] << subresult[3]
+         << endl;
 }
