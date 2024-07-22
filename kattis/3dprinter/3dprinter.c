60364334b3b4c0ffb8d8219f640ab150065f5750
JuliusSDev <julius.spiegelhalter@googlemail.com>
2024-07-22 03:03:40 +0200

    format

--- kattis/3dprinter/3dprinter.c
+++ kattis/3dprinter/3dprinter.c
@@ -1,7 +1,6 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-
 int main(void) {
     char input_buffer[10];
     fgets(input_buffer, sizeof(input_buffer), stdin);
@@ -12,22 +11,21 @@ int main(void) {
     int statues_printed = 0;
 
     while (statues_printed < statues_to_print) {
-        timestamps ++;
-        //printf("iteration: %d\n", timestamps);
-        if(2 * (printers + statues_printed) < statues_to_print){
-            //printf("case 1\n");
-            printers += printers; 
-        } else if ( statues_printed < statues_to_print) {
-          //  printf("case 2\n");
+        timestamps++;
+        // printf("iteration: %d\n", timestamps);
+        if (2 * (printers + statues_printed) < statues_to_print) {
+            // printf("case 1\n");
+            printers += printers;
+        } else if (statues_printed < statues_to_print) {
+            //  printf("case 2\n");
             statues_printed += printers;
         } else {
-          //  printf("case 3\n");
+            //  printf("case 3\n");
             printf("%d\n", timestamps);
             return 0;
-            
         }
     }
     printf("%d\n", timestamps);
-    
+
     return 0;
 }
