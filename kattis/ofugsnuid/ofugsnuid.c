213310940f9359cab467561213b7d282c230322d
JuliusSDev <131367704+JuliusSDev@users.noreply.github.com>
2024-09-08 13:09:24 +0200

    Kattis: replace makefile with cmake (#6)
    * add cmake files
    
    * added cmake
    
    * changed check.yml
    
    * change 3dprinter for cpp-standards
    
    * try to treat warnings as errors
    
    * change clang-tidy version to 12
    
    * remove Makefile due to cmake generation
    
    * added format comparison
    
    * applied format
    
    * ignore local CMakeFiles as a folder
    
    * clang-tidy -> clang-tidy-12
    
    * fixed fimmtudagstilbod
    
    * fixed for new clang-tidy standards

--- kattis/ofugsnuid/ofugsnuid.c
+++ kattis/ofugsnuid/ofugsnuid.c
@@ -1,20 +1,32 @@
-#include "stdio.h"
-#include "stdlib.h"
-int main(void) {
-    char input_buffer[8];
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-    int all_numbers[100000];
+#define MAX_INPUT_SIZE \
+    11 /* Max input number is 10^9 + 1 for the terminating symbol*/
+#define CONVERSION_BASE 10 /* We want decimal */
+
+int main(void) {
+    char input_buffer[MAX_INPUT_SIZE];
     fgets(input_buffer, sizeof(input_buffer), stdin);
+
+    char *endptr = NULL;
+    int size = (int)strtol(input_buffer, &endptr, CONVERSION_BASE);
+
+    int *all_numbers = (int *)malloc(size * sizeof(int));
+
     int index = 0;
-    for (int i = 0; i < atoi(input_buffer); i++) {
-        char in_2[32];
+    for (int i = 0; i < size; i++) {
+        char in_2[MAX_INPUT_SIZE];
         fgets(in_2, sizeof(in_2), stdin);
-        all_numbers[index] = atoi(in_2);
+        all_numbers[index] = (int)strtol(in_2, &endptr, CONVERSION_BASE);
         index++;
     }
+
     for (int i = index - 1; i >= 0; i--) {
         printf("%d\n", all_numbers[i]);
     }
 
+    free(all_numbers);
     return 0;
 }
